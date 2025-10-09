e20894aa52948d7773ae027f3d24d4ba41a38965
MrsVit <mitvz@yandex.ru>
2025-10-09 20:10:28 +0300

    lab2

---
+++ build/CMakeFiles/fc-tmp/googletest/download.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing download step for googletest")
+
+block(SCOPE_FOR VARIABLES)
+
+include("C:/Users/Angelok/Desktop/OOP_lab2/labsOOP_2/build/CMakeFiles/fc-tmp/googletest/googletest-gitclone.cmake")
+
+endblock()
---
+++ build/CMakeFiles/fc-tmp/googletest/update.cmake
@@ -0,0 +1,9 @@
+cmake_minimum_required(VERSION ${CMAKE_VERSION}) # this file comes with cmake
+
+message(VERBOSE "Executing update step for googletest")
+
+block(SCOPE_FOR VARIABLES)
+
+include("C:/Users/Angelok/Desktop/OOP_lab2/labsOOP_2/build/CMakeFiles/fc-tmp/googletest/googletest-gitupdate.cmake")
+
+endblock()
